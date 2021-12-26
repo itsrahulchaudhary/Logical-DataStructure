9daf562f396686ad75f0a3932ca7751df0155bd4
itsrahulchaudhary <rahulcy49@gmail.com>
2021-12-26 22:12:30 +0530

    array based prog...

---
+++ LogicalPrograms/src/main/java/in/rahulit/logical/arrays/A05KthLargestNo.java
@@ -0,0 +1,22 @@
+package in.rahulit.logical.arrays;
+
+public class A05KthLargestNo {
+	public static void main(String[] args) {
+		int arr[] = { 7, 3, 9, 1, 5, 6 };
+		int k = 3;
+		for (int i = 0; i < arr.length; i++) {
+			for (int j = i + 1; j < arr.length; j++) {
+				if (arr[i] < arr[j]) {
+					int temp = arr[i];
+					arr[i] = arr[j];
+					arr[j] = temp;
+				}
+			}
+			if (i == k - 1) {
+				System.out.println(k + " largest elements :: " + arr[i]);
+				break;
+			}
+		}
+	}
+
+}
